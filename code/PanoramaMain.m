9c0eb5e58652982ae09a69fe930b2de5d7f6464a
saikatgomes <saikat@cs.wisc.com>
2014-03-07 01:05:10 -0600

    added SIFT displays

--- code/PanoramaMain.m
+++ code/PanoramaMain.m
@@ -9,10 +9,12 @@ function [finalimg] = PanoramaMain(inDir, f)
     mkdir(outDir);
     display(strcat(datestr(now,'HH:MM:SS'),' [INFO] ', ...
         ' Output dir created at ',outDir));
-  
-    srcFiles = dir(strcat(inDir,'*.*'));  
+    
+    srcFiles = dir(strcat(inDir,'*.*')); 
+    count=0;
     for i = 1 : length(srcFiles)
         if (srcFiles(i).isdir~=1)
+            count=count+1;
             inFilename = strcat(inDir,srcFiles(i).name);            
             display(strcat(datestr(now,'HH:MM:SS'),' [INFO] ', ...
                 ' Processing file>',inFilename));            
@@ -20,6 +22,13 @@ function [finalimg] = PanoramaMain(inDir, f)
             NewI = convertToCylindrical(I,f);
             outFilename=strcat(outDir,'/cy_',srcFiles(i).name);  
             
+            if(count==1)
+                I1=NewI;
+            end
+            
+            if(count==2)
+                I2=NewI;
+            end
             
             
             display(strcat(datestr(now,'HH:MM:SS'),' [INFO] ', ...
@@ -27,6 +36,100 @@ function [finalimg] = PanoramaMain(inDir, f)
             imwrite(NewI,outFilename);
         end
     end
+    
+    srgStartup;
+    figure
+    I1 = single(rgb2gray(I1)) ;
+    [f,d] = vl_sift(I1) ;
+    
+    imshow(uint8(I1));
+    
+    hold on
+    
+    perm = randperm(size(f,2)) ;
+    
+    %display(perm);
+    sel = perm(1:100) ;
+    %display(sel);
+    
+    h1 = vl_plotframe(f(:,sel)) ;
+    h2 = vl_plotframe(f(:,sel)) ;
+    
+    set(h1,'color','k','linewidth',3) ;
+    set(h2,'color','y','linewidth',2) ;
+    
+    h3 = vl_plotsiftdescriptor(d(:,sel),f(:,sel)) ;
+    set(h3,'color','g') ;
+    
+    hold off;
+    
+    
+    
+    figure
+    I2 = single(rgb2gray(I2)) ;
+    [f2,d2] = vl_sift(I2) ;
+    
+    imshow(uint8(I2));
+    
+    hold on
+    
+    perm2 = randperm(size(f2,2)) ;
+    
+    %display(perm);
+    sel2 = perm2(1:100) ;
+    %display(sel);
+    
+    h1 = vl_plotframe(f2(:,sel2)) ;
+    h2 = vl_plotframe(f2(:,sel2)) ;
+    
+    set(h1,'color','k','linewidth',3) ;
+    set(h2,'color','y','linewidth',2) ;
+    
+    h3 = vl_plotsiftdescriptor(d(:,sel2),f2(:,sel2)) ;
+    set(h3,'color','g') ;
+    
+    hold off;
+    
+    
+    [matches, scores] = vl_ubcmatch(d, d2) ;
+    
+    display(matches);
+    display(scores);
+    
+% % % % % %     figure
+% % % % % %     
+% % % % % %     I2 = single(rgb2gray(I2)) ;
+% % % % % %     [f2,d2] = vl_sift(I2) ;
+% % % % % %     
+% % % % % %     perm2 = randperm(size(f2,2)) ;
+% % % % % %     
+% % % % % %     display(perm2);
+% % % % % %     sel2 = perm2(1:50) ;
+% % % % % %     display(sel2);
+% % % % % %     
+% % % % % %     h4 = vl_plotframe(f2(:,sel2)) ;
+% % % % % %     h5 = vl_plotframe(f2(:,sel2)) ;
+% % % % % %     
+% % % % % %     set(h4,'color','k','linewidth',3) ;
+% % % % % %     set(h5,'color','y','linewidth',2) ;
+% % % % % %     
+% % % % % %     h6 = vl_plotsiftdescriptor(d2(:,sel2),f2(:,sel2)) ;
+% % % % % %     set(h6,'color','g') ;
+    
+    
+% % % %     gray1 = GetGrayImageFrom3DArray(I1);
+% % % %     gray2 = GetGrayImageFrom3DArray(I2);
+% % % %     imshow(uint8(gray1));
+% % % %     figure;
+% % % %     imshow(uint8(gray2));
+% % % %     
+% % % %     %the sift arrays will be 4 x featurenum arrays
+% % % %     sift1 = vl_sift(gray1);
+% % % %     sift2 = vl_sift(gray2);
+% % % %     
+% % % %     
+% % % %     matcharr = vl_ubcmatch(sift1, sift2, threshold);
+
     
 % % % 
 % % %     %TODO: run SiftAndRansac with various pointsToSample and epsilons and
---
+++ code/srgStartup.m
@@ -0,0 +1,7 @@
+function [ ] = startup()
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+run('vlf/toolbox/vl_setup');
+vl_setup demo;
+end
+

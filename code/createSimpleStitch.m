c999ab56b3ce2e42768dfe77a02444ed720a2af5
saikatgomes <saikat@cs.wisc.com>
2014-03-07 16:19:18 -0600

    start working on simple panorama

--- code/createSimpleStitch.m
+++ code/createSimpleStitch.m
@@ -1,4 +1,4 @@
-function [ outImg ] = createSimpleStitch( pixArray )
+function [ outImg ] = createSimpleStitch( pixArray, outDir )
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
 
